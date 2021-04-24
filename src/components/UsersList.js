b54dc15707e12173a55ededbb9f70026a8f8a980
musobekmadrimov <musobekmadrimov99@gmail.com>
2021-04-25 00:06:58 +0500

    How to use styles in React

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../libs/axios';
+import './UsersList.css';
+import {StyledInput, StyledTitle} from './input';
 
 export default class UsersList extends Component {
     state = {
@@ -8,18 +10,18 @@ export default class UsersList extends Component {
         job: ''
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.getUsersFromServer();
     }
 
     handleInput = (e) => {
-        const {name, value} = e.target;
-        this.setState({[name]: value})
+        const { name, value } = e.target;
+        this.setState({ [name]: value })
     }
 
     createUser = () => {
         const url = `/users`;
-        const {name, job} = this.state;
+        const { name, job } = this.state;
         // fetch(url, {
         //     method: 'POST',
         //     headers: {
@@ -30,8 +32,8 @@ export default class UsersList extends Component {
         // .then(res => res.json())
         // .then(data => this.setState(prevState => ({users: [...prevState.users, {...data, first_name: data.name, last_name: data.job}]})))
         // .catch(err => console.error(err));
-        axios.post(url, {name, job})
-        .then(({data}) => {this.setState(prevState => ({users: [...prevState.users, {...data, first_name: data.name, last_name: data.job}]}))})
+        axios.post(url, { name, job })
+            .then(({ data }) => { this.setState(prevState => ({ users: [...prevState.users, { ...data, first_name: data.name, last_name: data.job }] })) })
     }
 
     getUsersFromServer = (num = 1) => {
@@ -40,37 +42,41 @@ export default class UsersList extends Component {
         // .then(res => res.json())
         // .then(data => this.setState({users: data.data}))
         // .catch(err => console.error(err));
-        axios(url).then(({data}) => this.setState({users: data.data}))
+        axios(url).then(({ data }) => this.setState({ users: data.data }))
     }
     render() {
         console.log(this.state)
-        const {users} = this.state;
+        const { users } = this.state;
         return (
-            <div>
-                <h2>Users List</h2>
-                <div>
+            <div className="container">
+                <div className="createUser">
+                    <h2>Users List</h2>
                     <p>
-                    <input type="text" name="name" onChange={this.handleInput}/>
+                        <input type="text" name="name" onChange={this.handleInput} />
                     </p>
                     <p>
-                    <input type="text" name="job" onChange={this.handleInput}/>
+                        <input type="text" name="job" onChange={this.handleInput} />
                     </p>
                     <button onClick={this.createUser}>CREATE</button>
 
                 </div>
-                <ul>{
-                        users.map(item => {
-                            return(
-                                <li key={item.id}>
-                                    <img src={item.avatar} alt={item.first_name + item.last_name}/>
-                                    {`${item.first_name} ${item.last_name}`}
-                                </li>
-                            )
-                        })
-                    }
+                <ul className="list">{
+                    users.map(item => {
+                        return (
+                            <li key={item.id} className="list-item">
+                                <img src={item.avatar} alt={item.first_name + item.last_name} />
+                                {`${item.first_name} ${item.last_name}`}
+                            </li>
+                        )
+                    })
+                }
                 </ul>
                 <button onClick={() => this.getUsersFromServer(1)}>Page 1</button>
                 <button onClick={() => this.getUsersFromServer(2)}>Page 2</button>
+
+
+                <StyledTitle>Heading from styled-components </StyledTitle>
+                <StyledInput placeholder="Test input from styled-component"/>
 
             </div>
         )
---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+    border: 4px double red;
+    width: 100%;
+`;
+
+const StyledTitle = styled.h1`
+font-size: 50px;
+color: red
+`;
+
+export { StyledInput, StyledTitle };
